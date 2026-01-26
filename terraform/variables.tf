717c3e59d51c45b4036f3659bf514f40e55bcc12
SafiullahRattar <safiullahrattar313@gmail.com>
2026-01-26 11:15:00 -0500

    init

---
+++ terraform/variables.tf
@@ -0,0 +1,99 @@
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project, used as prefix for resources"
+  type        = string
+  default     = "infra-deployer"
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, staging, production)"
+  type        = string
+  default     = "production"
+
+  validation {
+    condition     = contains(["dev", "staging", "production"], var.environment)
+    error_message = "Environment must be one of: dev, staging, production."
+  }
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "container_image" {
+  description = "Docker image URI for the application"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Port the container listens on"
+  type        = number
+  default     = 8080
+}
+
+variable "desired_count" {
+  description = "Desired number of ECS tasks"
+  type        = number
+  default     = 2
+}
+
+variable "cpu" {
+  description = "CPU units for the ECS task (1024 = 1 vCPU)"
+  type        = number
+  default     = 256
+}
+
+variable "memory" {
+  description = "Memory in MiB for the ECS task"
+  type        = number
+  default     = 512
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_name" {
+  description = "Name of the PostgreSQL database"
+  type        = string
+  default     = "appdb"
+}
+
+variable "db_username" {
+  description = "Master username for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Master password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "enable_deletion_protection" {
+  description = "Enable deletion protection on critical resources"
+  type        = bool
+  default     = true
+}
+
+variable "log_retention_days" {
+  description = "CloudWatch log retention in days"
+  type        = number
+  default     = 30
+}
